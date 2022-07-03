83ea60ee23ce5d91c91507a875a7492547591e87
gigi <shekiladze.2001@gmail.com>
2022-07-03 22:23:37 +0400

    Add project files.

---
+++ GetTreeNodeLeaves/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GetTreeNodeLeaves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = CreateTreeNode(0);
+            root.left = CreateTreeNode(1);
+            root.right = CreateTreeNode(2);
+            root.left.left = CreateTreeNode(3);
+            root.left.right = CreateTreeNode(4);
+            root.right.middle = CreateTreeNode(5);
+            root.left.left.left = CreateTreeNode(6);
+            root.right.middle.left = CreateTreeNode(7);
+            root.right.middle.middle = CreateTreeNode(8);
+            root.right.middle.right = CreateTreeNode(9);
+            root.right.middle.middle.middle = CreateTreeNode(10);
+            root.right.middle.right.right = CreateTreeNode(11);
+
+            GetTreeNodeLeaves(root);
+        }
+        static void GetTreeNodeLeaves(TreeNode root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("your TreeNode is empty");
+            }
+            if (root.left == null && root.right == null && root.middle == null)
+            {
+                Console.WriteLine(root.data + " ");
+            }
+            if (root.left != null)
+            {
+                GetTreeNodeLeaves(root.left);
+            }
+            if(root.middle != null)
+            {
+                GetTreeNodeLeaves(root.middle);
+            }
+            if (root.right != null)
+            {
+                GetTreeNodeLeaves(root.right);
+            }
+        }
+
+        static TreeNode CreateTreeNode(int data)
+        {
+            TreeNode t = new TreeNode();
+            t.data = data;
+            t.left = null;
+            t.middle = null;
+            t.right = null;
+            return t;
+        }
+    }
+}
---
+++ GetTreeNodeLeaves/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetTreeNodeLeaves
+{
+    public class TreeNode
+    {
+        public int data;
+        public TreeNode left;
+        public TreeNode middle;
+        public TreeNode right;
+    }
+}
